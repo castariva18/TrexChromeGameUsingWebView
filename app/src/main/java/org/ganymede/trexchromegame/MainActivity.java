e1dd3a7455e1b27fad7ca36e4c16269de0b7402c
Belmiro <tengku13elmiro@gmail.com>
2020-01-07 15:30:28 +0700

    Initial commit

---
+++ app/src/main/java/org/ganymede/trexchromegame/MainActivity.java
@@ -0,0 +1,36 @@
+package org.ganymede.trexchromegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient()); // load site
+        webView.loadUrl("https://dino-chrome.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+        //use to back button
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
